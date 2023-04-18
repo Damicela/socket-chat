ccdfc82928fb6e7d58de9b48c14d988797197182
Damicela Peña <damicelapr@gmail.com>
2023-04-18 10:55:32 -0600

    first commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,59 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if(!params.has('nombre') || !params.has('sala')){
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesario');
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+// aqui es donde defino el nombre del listener 'entrarChat'
+    socket.emit('entrarChat', usuario, function(resp){
+        console.log('Usuarios conectados', resp);
+    });
+
+
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+// Escuchar cambios de usuarios
+// Cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+
+    console.log(personas);
+
+});
+
+// mensajes provados
+socket.on('mensajePrivado', function(mensaje){
+    console.log('Mensaje Privado:', mensaje);
+});
