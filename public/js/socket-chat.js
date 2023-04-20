bf55b0efb1b7ad32ba847b41fc9e7841af1210f2
Damicela Peña <damicelapr@gmail.com>
2023-04-20 15:47:35 -0600

    chat terminado en el curso

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -2,9 +2,9 @@ var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-if(!params.has('nombre') || !params.has('sala')){
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y sala son necesario');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
@@ -12,13 +12,16 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
-// aqui es donde defino el nombre del listener 'entrarChat'
-    socket.emit('entrarChat', usuario, function(resp){
-        console.log('Usuarios conectados', resp);
-    });
 
+    // aqui es donde defino el nombre del listener 'entrarChat'
+    socket.emit('entrarChat', usuario, function(resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
+    });
 
 });
 
@@ -30,30 +33,22 @@ socket.on('disconnect', function() {
 });
 
 
-// Enviar información
-// socket.emit('crearMensaje', {
-//     nombre: 'Fernando',
-//     mensaje: 'Hola Mundo'
-// }, function(resp) {
-//     console.log('respuesta server: ', resp);
-// });
-
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
-// Cuando un usuario entra o sale del chat
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
-// mensajes provados
-socket.on('mensajePrivado', function(mensaje){
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+
     console.log('Mensaje Privado:', mensaje);
+
 });
